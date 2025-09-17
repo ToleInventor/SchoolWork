1e6a68e04ab4f33261ee4b38d9bb7348953810b6
ToleInventor <168644336+ToleInventor@users.noreply.github.com>
2025-09-17 13:44:01 +0300

    Add files via upload

---
+++ Units Semester1.C
@@ -0,0 +1,24 @@
+//simple program to display all the units for this semester
+
+/*
+Name: Caxtone Kirigha Tole
+Reg. No.: CT100/G/26122/25
+Description: A simple program which will display all the units for semester one year one at KYU
+*/
+
+#include <stdio.h> //pre-processor directive
+
+//main function
+int main(){
+printf("1. Mathematics for science \n");
+printf("2. Programming and algorithms \n");
+printf("3. essentials of accounting \n");
+printf("4. Communication skills \n");
+printf("5. Fundamentals os ICT \n");
+printf("6. Computer Applications \n");
+printf("7. Analogue electronics \n");
+printf("8. Computer Systems and organisation\n");
+return 0;
+}
+
+//end of program
