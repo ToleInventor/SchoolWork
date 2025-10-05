3b138f8f1bbd3ff012606422d42d9a9ed7e9ac7c
ToleInventor <168644336+ToleInventor@users.noreply.github.com>
2025-10-05 19:41:52 +0300

    Add files via upload

---
+++ CountDown.cpp
@@ -0,0 +1,18 @@
+//A simple C program to print all the numbers from 100 down to 50 in descending order
+/*
+Name: TOLE CAXTONE KIRIGHA
+Reg. No.: CT100/G/26122/25
+Description: A simple C program to print all the numbers from 100 down to 50 in descending order
+*/
+
+#include <stdio.h>//pre processor directive
+
+//now main function here
+int main()
+{
+	for(int i = 100; i>=50; i--){
+		printf("%d", i);
+		printf("\n");
+	}
+	return 0;
+}
